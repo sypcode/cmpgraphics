9bf7fddacad7e5499a0fcd68e1927c5dbcaf1504
troes <troester.luca@t-online.de>
2026-05-04 15:26:14 +0200

    :

--- Testat2/scanline/PolygonRasterer.java
+++ Testat2/scanline/PolygonRasterer.java
@@ -26,7 +26,29 @@ public class PolygonRasterer {
 	 * Umsetzung des Scan-Line-Verfahrens
 	 */
 	public void rasterize() {
-		// TODO: Ihr Code hier...
+		for (int y = 0; y < height; y++) {
+			final int scanline = y;
+
+			for (Edge e : edgeTable) {
+				if (e.getyMin() == scanline) {
+					activeEdgeTable.add(new Edge(e));
+				}
+			}
+
+			activeEdgeTable.sort(Comparator.comparingDouble(e -> e.getxIntersect()));
+
+			for (int i = 0; i + 1 < activeEdgeTable.size(); i += 2) {
+				int x1 = (int) activeEdgeTable.get(i).getxIntersect();
+				int x2 = (int) activeEdgeTable.get(i + 1).getxIntersect();
+				graphics.drawLine(x1, y, x2, y);
+			}
+
+			activeEdgeTable.removeIf(e -> e.getyMax() == scanline);
+
+			for (Edge e : activeEdgeTable) {
+				e.setxIntersect(e.getxIntersect() + e.getmReci());
+			}
+		}
 	}
 
 	public void setGraphics(Graphics graphics) {
